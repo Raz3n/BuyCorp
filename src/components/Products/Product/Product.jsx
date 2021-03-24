dcc343046464b37f31a779865d20afa96ba5857e
Nelson <nbritob@hotmail.com>
2021-03-24 02:18:35 +0000

    adds fetchProducts from commerce api and replaces dummy data and now renders fetched data

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -9,25 +9,27 @@ import {
 } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
 import useStyles from "./styles";
-import Products from "../Products";
 
 export const Product = ({ product }) => {
   const classes = useStyles();
+
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={product.image} title={product.name} />
+      <CardMedia
+        className={classes.media}
+        image={product.media.source}
+        title={product.name}
+      />
       <CardContent>
         <div className={classes.cardContent}>
           <Typography variant="h5" gutterBottom>
             {product.name}
           </Typography>
           <Typography variant="h5" gutterBottom>
-            {product.price}
+            {product.price.formatted_with_symbol}
           </Typography>
         </div>
-        <Typography variant="body2" color="textSecondary">
-          {product.description}
-        </Typography>
+        <Typography dangerouslySetInnerHTML={{__html: product.description}} variant="body2" color="textSecondary"></Typography>
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
         <IconButton aria-label="Add to Cart">
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,20 +4,15 @@ import Product from "./Product/Product";
 import useStyles from "./styles";
 
 
-const inventory = [
-  { id: 1, name: "shoes", description: "Running shoes", price: "£30", image: "" },
-  { id: 2, name: "Macbook", description: "Apple Macbook", price: "£1,700", image: "" },
-];
-
-const Products = () => {
+const Products = ({products}) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
-        {inventory.map((inventoryItem) => (
-          <Grid item key={inventoryItem.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={inventoryItem} />
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
           </Grid>
         ))}
       </Grid>
